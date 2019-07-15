225a2d7fc58818f62782a808ab14d1a6b056fb07
davidkao41 <davidkao41@gmail.com>
2019-07-15 15:06:42 -0700

    improved toggle annotations

--- src/components/ToggleAnnotation.js
+++ src/components/ToggleAnnotation.js
@@ -5,121 +5,11 @@ class ToggleAnnotation extends Component {
         super(props);
         this.handleClick = this.handleClick.bind(this);
         this.state = {
-            show: "true"
+            show: "false",
+            args: [true, true, true, true, true],
         }
     }
-    componentDidMount() {
-        this.updateCanvas();
-    }
-    updateCanvas() {
-        const context = this.refs.canvas.getContext('2d');
-        var canvasSizex = 300;
-        var canvasSizey = 300;
-        var ann1x = canvasSizex * .48;
-        var ann1y = canvasSizey * .03;
-        var ann2x = canvasSizex * .65;
-        var ann2y = canvasSizey * .15;
-        var ann3x = canvasSizex * .53;
-        var ann3y = canvasSizey * .35;
-        var ann4x = canvasSizex * .73;
-        var ann4y = canvasSizey * .57;
-        var ann5x = canvasSizex * .04;
-        var ann5y = canvasSizey * .12;
-        var ann6x = canvasSizex * .1;
-        var ann6y = canvasSizey * .37;
-        var ann7x = canvasSizex * .15;
-        var ann7y = canvasSizey * .55;
-        var ann8x = canvasSizex * .2;
-        var ann8y = canvasSizey * .75;
-        //Central trachea
-        context.strokeStyle = "#5acc5a";
-        context.lineWidth = .5;
-        context.strokeRect(ann1x,ann1y,60,25);
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("Central Trachea", ann1x, ann1y*2.5);
-        //clear left lung
-        context.strokeStyle = "#5acc5a"
-        context.lineWidth = .5;
-        context.strokeRect(ann2x,ann2y, 55, 25);
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("Clear left lung", ann2x, ann2y*1.3);
-        //normal position of
-        context.strokeStyle = "#5acc5a"
-        context.lineWidth = .5;
-        context.strokeRect(ann3x,ann3y, 80, 40);
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("Normal position of", ann3x, ann3y*1.1);
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("the left mediastinal  ", ann3x, ann3y*1.1+8);
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("and cardiac contours ", ann3x, ann3y*1.1+16);
-        //minor blunting of the left
-        context.strokeStyle = "#5acc5a"
-        context.lineWidth = .5;
-        context.strokeRect(ann4x, ann4y, 75, 45);
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("Minor blunting of the", ann4x, ann4y*1.05);
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("left costophrenic", ann4x, ann4y*1.05+8);
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("angle", ann4x, ann4y*1.05+16);
-        //meniscus present
-        context.strokeStyle = "#5acc5a"
-        context.lineWidth = .5;
-        context.strokeRect(ann5x,ann5y, 65, 20);
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("Meniscus present", ann5x, ann5y*1.30);
-        //homogenous opacity
-        context.strokeStyle = "#5acc5a"
-        context.lineWidth = .5;
-        context.strokeRect(ann6x,ann6y, 75, 20);
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("Homogenous opacity", ann6x, ann6y*1.12);
-        //the right hemidiaphragm
-        context.strokeStyle = "#b0b0b0"
-        context.lineWidth = .5;
-        context.strokeRect(ann7x,ann7y, 75, 40);
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("The right", ann7x, ann7y*1.07);
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("hemidiaphragm", ann7x, ann7y*1.07+8);
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("and cardiac contour", ann7x, ann7y*1.07+16);
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("are not visible", ann7x, ann7y*1.07+24);
-        //obliteration
-        context.strokeStyle = "#b0b0b0"
-        context.lineWidth = .5;
-        context.strokeRect(ann8x,ann8y, 70, 40);
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("Obliteration of the", ann8x, ann8y*1.07); 
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("right costophrenic", ann8x, ann8y*1.07+8); 
-        context.font = "8px Arial";
-        context.textAlign = "left";
-        context.fillText("angle", ann8x, ann8y*1.07+16); 
-        
-
-        context.strokeStyle = "#d3d3d3"
-        context.lineWidth = 2;
-        context.strokeRect(0,0, 300, 300);
-    }
+    
     handleClick(){
         if(this.state.show === "false"){
             this.setState({show: "true"});
@@ -130,10 +20,24 @@ class ToggleAnnotation extends Component {
         }
     }
     render(){
+        var pleuralEffusion = (this.state.args[0] === true) ? "PleuralEffusion" : "false";
+        var airway = (this.state.args[1] === true) ? "Airway" : "false";
+        var breathing = (this.state.args[2] === true) ? "Breathing" : "false";
+        var cardiac = (this.state.args[3] === true) ? "Cardiac" : "false";
+        var unclear = (this.state.args[4] === true) ? "Unclear" : "false";
         return(
             <div className = "draw">
                 
-                <canvas className= {this.state.show} ref="canvas" width="300" height="300"> </canvas>
+                {/*<canvas className= {this.state.show} ref="canvas" width="300" height="300"> </canvas>*/}
+                <textarea className= { `${this.state.show} ${"a1"} ${this.airway}`} rows="2" cols="6" disabled> Central Trachea </textarea>
+                <textarea className= { `${this.state.show} ${"a2"} ${this.breathing}`} rows="2" cols="7" disabled> Clear left lung </textarea>
+                <textarea className= { `${this.state.show} ${"a3"} ${this.cardiac}`} rows="4" cols="13" disabled> Normal position of the left mediastinal and cardiac contours </textarea>
+                <textarea className= { `${this.state.show} ${"a4"} ${this.breathing} ${this.pleuralEffusion}`} rows="4" cols="12" disabled> Minor blunting of the left costophrenic angle</textarea>
+                <textarea className= { `${this.state.show} ${"a5"} ${this.breathing} ${this.pleuralEffusion}`} rows="2" cols="7" disabled> Meniscus present</textarea>
+                <textarea className= { `${this.state.show} ${"a6"} ${this.breathing} ${this.pleuralEffusion}`} rows="2" cols="8" disabled> Homogenous opacity </textarea>
+                <textarea className= { `${this.state.show} ${"a7"} ${this.unclear}`} rows="4" cols="14" disabled> The right hemidiaphragm and cardiac contour are not visible</textarea>
+                <textarea className= { `${this.state.show} ${"a8"} ${this.unclear}`} rows="4" cols="10" disabled> Obliteration of the right costophrenic angle </textarea>
+
                 <button className = "toggle" onClick = {() => this.handleClick()}> Toggle Annotation </button>
             </div>
         );
